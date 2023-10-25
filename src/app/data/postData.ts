fcf139d7712693c9c3feffd50796e54221eb46bb
João <povoajoao6@gmail.com>
2023-10-24 21:57:38 -0300

    PUSHING FILE FROM DEVELOP B

---
+++ src/app/data/postData.ts
@@ -0,0 +1,185 @@
+import { Post } from "../types/PostsType"
+
+export const postData: Post[] = [
+  {
+    "id": 1,
+    "userId": 101,
+    "post": "This is the first post.",
+    "postedBy": "User A",
+    "date": "2023-10-24T09:00:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+  },
+  {
+    "id": 2,
+    "userId": 102,
+    "post": "Another post by User B.",
+    "postedBy": "User B",
+    "date": "2023-10-24T09:15:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+  },
+  {
+    "id": 3,
+    "userId": 103,
+    "post": "User C shares their thoughts here.",
+    "postedBy": "User C",
+    "date": "2023-10-24T09:30:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+  },
+  {
+    "id": 4,
+    "userId": 104,
+    "post": "User D with a new post.",
+    "postedBy": "User D",
+    "date": "2023-10-24T09:45:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+  },
+  {
+    "id": 5,
+    "userId": 105,
+    "post": "Fifth post in the list.",
+    "postedBy": "User E",
+    "date": "2023-10-24T10:00:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 6,
+    "userId": 106,
+    "post": "User F shares something interesting.",
+    "postedBy": "User F",
+    "date": "2023-10-24T10:15:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 7,
+    "userId": 107,
+    "post": "User G's post for today.",
+    "postedBy": "User G",
+    "date": "2023-10-24T10:30:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 8,
+    "userId": 108,
+    "post": "Post number 8 here.",
+    "postedBy": "User H",
+    "date": "2023-10-24T10:45:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 9,
+    "userId": 109,
+    "post": "Ninth post by User I.",
+    "postedBy": "User I",
+    "date": "2023-10-24T11:00:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 10,
+    "userId": 110,
+    "post": "User J's latest post.",
+    "postedBy": "User J",
+    "date": "2023-10-24T11:15:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 11,
+    "userId": 111,
+    "post": "Eleventh post in the series.",
+    "postedBy": "User K",
+    "date": "2023-10-24T11:30:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 12,
+    "userId": 112,
+    "post": "User L with a new update.",
+    "postedBy": "User L",
+    "date": "2023-10-24T11:45:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 13,
+    "userId": 113,
+    "post": "User M's post for today.",
+    "postedBy": "User M",
+    "date": "2023-10-24T12:00:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 14,
+    "userId": 114,
+    "post": "Fourteenth post in the list.",
+    "postedBy": "User N",
+    "date": "2023-10-24T12:15:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 15,
+    "userId": 115,
+    "post": "User O shares their thoughts here.",
+    "postedBy": "User O",
+    "date": "2023-10-24T12:30:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 16,
+    "userId": 116,
+    "post": "User P with a new post.",
+    "postedBy": "User P",
+    "date": "2023-10-24T12:45:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 17,
+    "userId": 117,
+    "post": "Seventeenth post by User Q.",
+    "postedBy": "User Q",
+    "date": "2023-10-24T13:00:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 18,
+    "userId": 118,
+    "post": "User R's post for today.",
+    "postedBy": "User R",
+    "date": "2023-10-24T13:15:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 19,
+    "userId": 119,
+    "post": "User S shares something interesting.",
+    "postedBy": "User S",
+    "date": "2023-10-24T13:30:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  },
+  {
+    "id": 20,
+    "userId": 120,
+    "post": "Twentieth post in the list.",
+    "postedBy": "User T",
+    "date": "2023-10-24T13:45:00",
+    "avatar": "https://st3.depositphotos.com/1017228/18878/i/450/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+
+  }
+]
+
+
+export const addNewPost = (newPost: Post) => {
+  postData.push(newPost);
+};
